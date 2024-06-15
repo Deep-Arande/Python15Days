39dc5765d7103b51727b21a1c9b6e83b5e87520f
Deep-Arande <deeparande29@gmail.com>
2024-06-15 20:17:22 +0530

    Added Day 1 folder

---
+++ Day1/AreaOfRectangle.py
@@ -0,0 +1,10 @@
+#a Python program to calculate the area of a
+#rectangle given its length and width
+print("Enter the width of rectangle:",end=" ")
+width=int(input())
+
+print("Enter the height of rectangle:",end=" ")
+height=int(input())
+
+
+print(f"Area of rectangle is: {width*height}")
